1762f6d981825695bc5500f7fe4daf53dda0110a
Anush S Jathan <anushjathan268@gmail.com>
2025-07-27 15:31:53 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
-python -c "import nltk; nltk.download('punkt')"
-import nltk; nltk.download('punkt')
 
 pip install -r requirements.txt
+python -c "import nltk; nltk.download('punkt')"
 python manage.py collectstatic --no-input
 python manage.py migrate
